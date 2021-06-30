719ec79253ca4a67d5bc87ebbd5f98beca9a114f
Alex <boychev46@gmail.com>
2021-06-30 10:06:41 +0300

    add styles for datacapture

--- webpackapp/app/components/DataCapture.jsx
+++ webpackapp/app/components/DataCapture.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../../public/css/DataCapture.module.css'
 export default class DataCapture extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state = { 
+        this.state = {
             _firstName: this.props.firstName,
             _lastName: this.props.lastName,
             _dateOfBirth: this.props.dateOfBirth,
@@ -13,22 +15,31 @@ export default class DataCapture extends React.Component {
     }
 
     render() {
-        return( 
-        <div>
-            <div>
-                {/* first block for image user */}
-            </div>
-            <div>
-                {/* second block for info user and button <changeInfo> - change user data*/}
-            <p>Name: {this.state._firstName}</p>
-            <p>Surname: {this.state._lastName}</p>
-            <p>Date of birth: {this.state._dateOfBirth}</p>
-            <p>Gender: {this.state._gender}</p>
-            <p>About me: {this.state._aboutMe}</p>
-            </div>
-            <div>
-                {/* third block burger menu 3*/}
-            </div>            
-        </div>);
+        return (
+            <div className="mainDataCaptureBlock">
+                <header className="burgerBlock">
+                    {/* third block burger menu 3*/}
+                </header>
+                <div className="imgBlock">
+                    {/* first block for image user */}
+                    <img src="../../public/src/noFace.jpg"></img>
+                </div>
+                <div className="infoBlock">
+                    {/* second block for info user and button <changeInfo> - change user data*/}
+                    <p>Name: {this.state._firstName}</p>
+                    <p>Surname: {this.state._lastName}</p>
+                    <p>Date of birth: {this.state._dateOfBirth}</p>
+                    <p>Gender: {this.state._gender}</p>
+                    <p>About me: {this.state._aboutMe}</p>
+                </div>
+
+                <div className="wallBlock">
+                </div>
+                <div className="changeBlock">
+                    <Link to="/changedata" type="button" className="btn btn-primary ">Change</Link>
+                </div>
+                <footer className="footerBlock">
+                </footer>
+            </div>);
     }
 }
